75841dafdb777accf620f13ca381c9ad30c1776e
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-05-21 17:54:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,11 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
+BOT_NAME = getenv("BOT_NAME", "reklam") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
