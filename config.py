6463ccf7053954766e746e1ff187eeed4a8e5906
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-05-21 19:14:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "reklam") 
+BOT_NAME = getenv("BOT_NAME", "reklamcı") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
